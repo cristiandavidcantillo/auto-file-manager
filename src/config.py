18b0f6296ed023104b203a2d9208da7129cfa44b
Cristian cantillo <cantilloc867@gmail.com>
2025-03-28 20:05:00 -0500

    Creación automatica carpetas

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
 import os
 
+#carpeta monitoreada en busca de archivos nuevos
 WATCH_FOLDER = r"C:\Users\Usuario\Downloads"
+
+#carpeta donde se moverán los archivos
 DESTINATION_FOLDER = r"C:\Users\Usuario\Documents\AutoSort"
+
 
 FILE_TYPES = {
     "Imágenes": [".jpg", ".png", ".jpeg", ".gif"],
@@ -10,3 +14,12 @@ FILE_TYPES = {
     "Música": [".mp3", ".wav"],
     "Otros": []
 }
+
+#Creación automática de carpetas si no existen
+def create_folders():
+    for folder in FILE_TYPES.keys():    #iterar cada carpeta
+        path = os.path.join(DESTINATION_FOLDER, folder)
+        os.makedirs(path, exist_ok=True)
+
+create_folders()
+
